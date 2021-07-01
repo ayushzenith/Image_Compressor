308cc20070ce5183057b6c5992af583f6518046e
ayushzenith <ayushp.zenith@gmail.com>
2021-07-01 02:36:38 -0400

    Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Requirements
+* [Crunch](https://github.com/chrissimpkins/Crunch/blob/master/docs/EXECUTABLE.md)
+* Pngcrunch
+* Gifsicle (might also need giflossy)
+* Jpegtran
+* Jpegoptim
+
+# Running
+
+lossy("input/png.png","output/lossypng.png")
+lossless("input/png.png","output/losslesspng.png")
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import os
+import shutil
+from subprocess import call
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def lossy(source, destination):
+    filename = os.path.splitext(source)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if filename[1] == ".jpeg" or filename[1] == ".jpg":
+        shutil.copyfile(source, destination)
+        call(["jpegoptim","--max=75", destination])
+    elif filename[1] == ".png":
+        call(["crunch", source])
+        os.rename(filename[0]+"-crunch"+filename[1],destination)
+    elif filename[1] == ".gif":
+        call(["gifsicle", '-O3', '--lossy=75',source, "--output",destination])
+    else:
+        print("filetype not recognized")
+
+
+
+def lossless(source, destination):
+    filename = os.path.splitext(source)
+
+    if filename[1] == ".jpeg" or filename[1] == ".jpg":
+        call(["jpegtran", '-copy', 'exif', '-optimize', '-perfect', '-outfile', source, destination])
+    elif filename[1] == ".png":
+        call(["pngcrush", '-rem', 'alla', '-reduce', '-brute', source, destination])
+    elif filename[1] == ".gif":
+        call(["gifsicle", '-o3', source, "--output", destination])
+    else:
+        print("filetype not recognized")
+
+lossy("input/png.png","output/lossypng.png")
+lossless("input/png.png","output/losslesspng.png")
+
+
